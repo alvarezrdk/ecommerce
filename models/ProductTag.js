6c21d640699a84ca3b129476711595cecd124d35
Ruben <rubend.alvarezc@gmail.com>
2023-10-11 23:26:36 -0400

    product tag fields

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const { INTEGER } = require('sequelize');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: INTEGER,
+      references: {
+        model: "Product",
+        key: "id"
+      }
+    },
+    tag_id: {
+      type: INTEGER,
+      references: {
+        model: "Tag",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
