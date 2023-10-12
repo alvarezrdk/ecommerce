b17faa0155f4510680029a24343cc2a2f77706c6
Ruben <rubend.alvarezc@gmail.com>
2023-10-11 23:23:21 -0400

    tag fields

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,23 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INET } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { INTEGER } = require('sequelize');
+const { STRING } = require('sequelize');
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: STRING
+    }
   },
   {
     sequelize,
