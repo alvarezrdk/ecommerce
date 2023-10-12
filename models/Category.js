c60c8561d8d229c712bc751ef0106d86d01205fd
Ruben <rubend.alvarezc@gmail.com>
2023-10-11 23:39:07 -0400

    Category fields

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { INTEGER } = require('sequelize');
+const { STRING } = require('sequelize');
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: STRING,
+      allowNull:false
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -17,14 +17,14 @@ ProductTag.init(
     product_id: {
       type: INTEGER,
       references: {
-        model: "Product",
+        model: "product",
         key: "id"
       }
     },
     tag_id: {
       type: INTEGER,
       references: {
-        model: "Tag",
+        model: "tag",
         key: "id"
       }
     }
